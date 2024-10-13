0d25c7072355f5fce45ea98bb0bcedb9fdff61b8
Tom Ashworth <tgvashworth@gmail.com>
2024-10-13 18:49:34 +0000

    Initial commit of basic models

--- gyrinx/core/models.py
+++ gyrinx/core/models.py
@@ -1,1 +1,24 @@
-# Create your models here.
+import uuid
+
+from django.db import models
+
+
+class Base(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Content(Base):
+    filepath = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class ContentArchetype(Content):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
