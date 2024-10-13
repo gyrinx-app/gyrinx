ecd8be0a9f951ef93be76c63d451e072e99bac97
Tom Ashworth <tgvashworth@gmail.com>
2024-10-13 14:34:41 +0000

    Fix tests by adding schema and id

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,13 +3,27 @@ from pathlib import Path
 from src.schema import DataSource, Schema, validate_sources
 
 
+def mk_schema(name: str, schema: dict) -> Schema:
+    return Schema(
+        name,
+        Path(f"{name}.schema.json"),
+        {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "$id": f"urn:{name}",
+            **schema,
+        },
+    )
+
+
 def test_basic():
     result = validate_sources(
         schemas={
-            "test": Schema(
+            "test": mk_schema(
                 "test",
-                Path("test.schema.json"),
-                {"type": "object", "properties": {"name": {"type": "string"}}},
+                {
+                    "type": "object",
+                    "properties": {"name": {"type": "string"}},
+                },
             )
         },
         data_sources=[DataSource("test", Path("data/test.yaml"), [{"name": "test"}])],
@@ -21,10 +35,12 @@ def test_basic():
 def test_basic_failing():
     result = validate_sources(
         schemas={
-            "test": Schema(
+            "test": mk_schema(
                 "test",
-                Path("test.schema.json"),
-                {"type": "object", "properties": {"name": {"type": "string"}}},
+                {
+                    "type": "object",
+                    "properties": {"name": {"type": "string"}},
+                },
             )
         },
         data_sources=[DataSource("test", Path("data/test.yaml"), [{"name": 1}])],
@@ -35,10 +51,12 @@ def test_basic_failing():
 def test_source_without_schema():
     result = validate_sources(
         schemas={
-            "test": Schema(
+            "test": mk_schema(
                 "test",
-                Path("test.schema.json"),
-                {"type": "object", "properties": {"name": {"type": "string"}}},
+                {
+                    "type": "object",
+                    "properties": {"name": {"type": "string"}},
+                },
             )
         },
         data_sources=[
