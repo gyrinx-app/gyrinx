0d5dbc29c391878dbdd7bd7674b0cf0c2a98fdb5
Tom Ashworth <tgvashworth@gmail.com>
2024-11-03 11:50:43 +0000

    Add importer functionality for removal and changes

--- gyrinx/content/management/imports.py
+++ gyrinx/content/management/imports.py
@@ -16,6 +16,7 @@ class ImportConfig:
     id: callable
     model: models.Model
     fields: callable
+    allow_removal: bool = False
 
 
 class Importer:
@@ -37,16 +38,42 @@ class Importer:
     def do(self, config: ImportConfig, data_sources: list[DataSource]):
         data = data_for_type(config.source, data_sources)
         click.echo(f"Found {len(data)} for {config.source}: ")
+
+        # Compare the imported data to the existing objects
+        # and check for issues.
+        all_existing = config.model.objects.all()
+        if all_existing:
+            # Check for removal
+            if len(all_existing) > len(data):
+                click.echo(
+                    f"Warning: Found {len(all_existing)} existing {config.model} objects, but only {len(data)} in the data sources."
+                )
+                if not config.allow_removal:
+                    raise ValueError(
+                        "Data sources do not match existing objects: something was removed and allow_removal is False."
+                    )
+
+            # Check for changes in the ID fields — this is a breaking change so we error
+            existing_uuids = set(obj.uuid for obj in all_existing)
+            new_uuids = set(stable_uuid(config.id(d)) for d in data)
+            if len(all_existing) == len(data) and existing_uuids - new_uuids:
+                if not config.allow_removal:
+                    raise ValueError(
+                        "Data sources do not match existing objects: an ID field has changed."
+                    )
+
+        suuids = set()
         for d in data:
             suuid = stable_uuid(config.id(d))
+            suuids.add(suuid)
+            # TODO: Check for duplicate UUIDs
             obj = config.model.objects.filter(uuid=suuid).first()
             if obj:
                 click.echo(f" - Existing: {config.id(d)} ({obj.uuid}, {obj.version})")
 
-                obj.update(
-                    version=self.iv,
-                    **config.fields(d),
-                )
+                obj.version = self.iv
+                for k, v in config.fields(d).items():
+                    setattr(obj, k, v)
             else:
                 obj = config.model(
                     version=self.iv,
@@ -57,3 +84,10 @@ class Importer:
             click.echo(f" - {obj} ({obj.uuid}, {obj.version})")
             if not self.dry_run:
                 obj.save()
+
+        if config.allow_removal:
+            for obj in all_existing:
+                if obj.uuid not in suuids:
+                    click.echo(f" - Removing: {obj} ({obj.uuid}, {obj.version})")
+                    if not self.dry_run:
+                        obj.delete()
