c3eeb5928dd535fda6652cf7542f0421f97c7e13
Tom Ashworth <tgvashworth@gmail.com>
2024-11-30 12:18:45 +0000

    Sort fighter & equipment list

--- gyrinx/core/views.py
+++ gyrinx/core/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Case, When
 from django.shortcuts import get_list_or_404, render
 
 from gyrinx.content.models import ContentEquipment, ContentHouse
@@ -10,13 +11,32 @@ def index(request):
 def content(request):
     houses = get_list_or_404(ContentHouse)
     for house in houses:
-        house.fighters = list(house.contentfighter_set.all())
+        house.fighters = list(
+            house.contentfighter_set.all().order_by(
+                Case(
+                    When(category="LEADER", then=0),
+                    When(category="CHAMPION", then=1),
+                    When(category="PROSPECT", then=2),
+                    When(category="JUVE", then=3),
+                    default=99,
+                ),
+                "type",
+            )
+        )
 
     equipment = get_list_or_404(ContentEquipment)
     categories = {}
     for item in equipment:
         category = item.cat()
-        item.profiles = list(item.contentweaponprofile_set.all())
+        item.profiles = list(
+            item.contentweaponprofile_set.all().order_by(
+                Case(
+                    When(name="", then=0),
+                    default=1,
+                ),
+                "name",
+            )
+        )
         if category not in categories:
             categories[category] = []
         categories[category].append(item)
