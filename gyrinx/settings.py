925f30700c744f17646d5cf0c0537828ead26137
Tom Ashworth <tgvashworth@gmail.com>
2024-12-12 22:00:25 +0000

    Add email backend config

--- gyrinx/settings.py
+++ gyrinx/settings.py
@@ -55,6 +55,11 @@ logger.info(f"CSRF_COOKIE_DOMAIN: {CSRF_COOKIE_DOMAIN}")
 # Email
 # Use SMTP
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Application definition
 
