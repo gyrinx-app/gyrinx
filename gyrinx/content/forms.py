d981e2d1f1074cfdc3b3d4589dbb8d0f151d1c83
Tom Ashworth <tgvashworth@gmail.com>
2025-01-12 12:38:35 +0000

    Add copy Equipment List Item to ContentFighter

---
+++ gyrinx/content/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from gyrinx.content.models import ContentFighter
+
+
+class CopyEquipmentListItemForm(forms.Form):
+    _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+    to_fighters = forms.ModelMultipleChoiceField(
+        ContentFighter.objects, label="To ContentFighters:"
+    )
