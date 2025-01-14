78e35306c20bc3373801d0bf72b4c5e4952cb257
Tom Ashworth <tgvashworth@gmail.com>
2025-01-14 21:13:53 +0000

    Content migration for skill categories

--- gyrinx/content/tests/test_migrations.py
+++ gyrinx/content/tests/test_migrations.py
@@ -1,22 +1,6 @@
-import json
-from pathlib import Path
-
 import pytest
 
-from gyrinx.content.models import ContentBook, ContentPageRef, ContentSkill
-
-
-@pytest.mark.django_db
-def test_skills_migration():
-    root = Path(__file__).parent / "../../../content/exports"
-    top_skills_path = root / "top_skills.json"
-    with open(top_skills_path, "r") as file:
-        top_skills = json.load(file)
-
-    for skill in top_skills[0:100]:
-        assert ContentSkill.objects.filter(name=skill["skill"]).exists(), (
-            f"Skill {skill['skill']} not found"
-        )
+from gyrinx.content.models import ContentBook, ContentPageRef
 
 
 @pytest.mark.django_db
