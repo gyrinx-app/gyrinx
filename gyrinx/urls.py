13ac8535a8b0dbe706520399809b29a1432b374a
Tom Ashworth <tgvashworth@gmail.com>
2025-01-21 22:03:45 +0000

    Drop feedback routing

--- gyrinx/urls.py
+++ gyrinx/urls.py
@@ -22,7 +22,6 @@ from gyrinx.pages import views
 
 urlpatterns = [
     path("", include("gyrinx.core.urls")),
-    path("feedback/", include("gyrinx.feedback.urls")),
     path("accounts/", include("allauth.urls")),
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
