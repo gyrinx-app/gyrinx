2af18f810c0f57653703c83372264ab247d7c38c
Tom Ashworth <tgvashworth@gmail.com>
2025-02-08 19:26:06 +0000

    Add some info to webhook requests

--- gyrinx/api/admin.py
+++ gyrinx/api/admin.py
@@ -3,9 +3,36 @@ from django.contrib import admin
 from gyrinx.api.models import WebhookRequest
 
 
+def payload(key, description=None):
+    if description is None:
+        description = key
+
+    segments = key.split(".")
+
+    @admin.display(description=description)
+    def _payload(obj):
+        curr = obj.payload
+        for segment in segments:
+            if segment not in curr:
+                return None
+
+            curr = curr.get(segment, {})
+        return curr
+
+    return _payload
+
+
 @admin.register(WebhookRequest)
 class WebhookRequestAdmin(admin.ModelAdmin):
     search_fields = ["source", "event"]
     list_display = ["source", "event", "created"]
-    readonly_fields = ["source", "event", "payload", "signature", "created"]
+    readonly_fields = [
+        "source",
+        "event",
+        payload("data.attributes.email", "Email"),
+        payload("data.attributes.full_name", "Name"),
+        "payload",
+        "signature",
+        "created",
+    ]
     list_filter = ["source", "event"]
