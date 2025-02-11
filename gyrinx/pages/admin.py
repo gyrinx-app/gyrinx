131589da10daa9b923ee7f01f37ac0b586a6043a
Tom Ashworth <tgvashworth@gmail.com>
2025-02-11 08:39:43 +0000

    Upgrade TinyMCE for flatpages

--- gyrinx/pages/admin.py
+++ gyrinx/pages/admin.py
@@ -17,8 +17,56 @@ class FlatPageAdmin(BaseFlatPageAdmin):
         if db_field.name == "content":
             return db_field.formfield(
                 widget=TinyMCE(
-                    attrs={"cols": 80, "rows": 30},
-                    mce_attrs={"external_link_list_url": reverse("tinymce-linklist")},
+                    attrs={"cols": 120, "rows": 30},
+                    mce_attrs={
+                        "promotion": False,
+                        "resize": "both",
+                        "width": "100%",
+                        "height": "66vh",
+                        "external_link_list_url": reverse("tinymce-linklist"),
+                        "plugins": "code textpattern",
+                        "toolbar": "undo redo | bold italic | code",
+                        "menubar": "edit view insert format table tools",
+                        "menu": {
+                            "edit": {
+                                "title": "Edit",
+                                "items": "undo redo | cut copy paste pastetext | selectall | searchreplace",
+                            },
+                            "view": {
+                                "title": "View",
+                                "items": "code revisionhistory | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments",
+                            },
+                            "insert": {
+                                "title": "Insert",
+                                "items": "image link media addcomment pageembed codesample inserttable | math | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime",
+                            },
+                            "format": {
+                                "title": "Format",
+                                "items": "bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat",
+                            },
+                            "tools": {
+                                "title": "Tools",
+                                "items": "spellchecker spellcheckerlanguage | a11ycheck code wordcount",
+                            },
+                            "table": {
+                                "title": "Table",
+                                "items": "inserttable | cell row column | advtablesort | tableprops deletetable",
+                            },
+                        },
+                        "textpattern_patterns": [
+                            {"start": "# ", "replacement": "<h1>%</h1>"},
+                            {"start": "## ", "replacement": "<h2>%</h2>"},
+                            {"start": "### ", "replacement": "<h3>%</h3>"},
+                            {"start": "#### ", "replacement": "<h4>%</h4>"},
+                            {"start": "##### ", "replacement": "<h5>%</h5>"},
+                            {"start": "###### ", "replacement": "<h6>%</h6>"},
+                            {
+                                "start": r"\*\*([^\*]+)\*\*",
+                                "replacement": "<strong>%</strong>",
+                            },
+                            {"start": r"\*([^\*]+)\*", "replacement": "<em>%</em>"},
+                        ],
+                    },
                 )
             )
         return super().formfield_for_dbfield(db_field, **kwargs)
