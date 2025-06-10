f6b23dd1c95003cc501e0f55d0cf344c507821b3
Tom Ashworth <tgvashworth@gmail.com>
2025-06-10 11:01:57 +0100

    GitHub workflow file fixes

--- scripts/check_migrations.sh
+++ scripts/check_migrations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-manage makemigrations --check --dry-run --verbosity 0
+set -e
+
+python scripts/manage.py makemigrations --check --dry-run --verbosity 0
 if [ $? -ne 0 ]; then
     echo "Migrations are not up to date. Please run 'manage makemigrations' to create new migrations."
     exit 1
