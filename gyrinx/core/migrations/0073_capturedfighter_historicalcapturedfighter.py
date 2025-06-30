4f7c9e5d5bc80cd1b5e7ecc3502d05fc8e75fba5
Tom Ashworth <tgvashworth@gmail.com>
2025-06-30 11:45:13 +0100

    Fixes and tweaks

--- gyrinx/core/migrations/0073_capturedfighter_historicalcapturedfighter.py
+++ gyrinx/core/migrations/0073_capturedfighter_historicalcapturedfighter.py
@@ -8,7 +8,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("core", "0072_add_missing_database_indexes"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
