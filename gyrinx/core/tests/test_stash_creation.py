d25403e9d067fce3a1fb5505a19bfafb267f840d
Tom Ashworth <tgvashworth@gmail.com>
2025-07-06 19:55:17 +0100

    Fmt

--- gyrinx/core/tests/test_stash_creation.py
+++ gyrinx/core/tests/test_stash_creation.py
@@ -12,14 +12,14 @@ def test_new_list_creates_stash_fighter():
     """Test that creating a new list automatically creates a stash fighter"""
     # Create a user
     user = User.objects.create_user(username="testuser", password="testpass")
-    
+
     # Create a house
     house = ContentHouse.objects.create(name="Test House")
-    
+
     # Login
     client = Client()
     client.login(username="testuser", password="testpass")
-    
+
     # Create a new list
     response = client.post(
         reverse("core:lists-new"),
@@ -29,15 +29,15 @@ def test_new_list_creates_stash_fighter():
             "public": False,
         },
     )
-    
+
     # Check redirect
     assert response.status_code == 302
-    
+
     # Check list was created
     list_ = List.objects.get(name="Test List")
     assert list_.owner == user
     assert list_.content_house == house
-    
+
     # Check stash fighter was created
     stash_fighter = ListFighter.objects.get(list=list_, content_fighter__is_stash=True)
     assert stash_fighter.name == "Stash"
@@ -53,21 +53,21 @@ def test_show_stash_adds_stash_fighter():
     user = User.objects.create_user(username="testuser", password="testpass")
     house = ContentHouse.objects.create(name="Test House")
     list_ = List.objects.create(name="Test List", content_house=house, owner=user)
-    
+
     # Verify no stash fighter initially
     assert not list_.listfighter_set.filter(content_fighter__is_stash=True).exists()
-    
+
     # Login
     client = Client()
     client.login(username="testuser", password="testpass")
-    
+
     # Call show_stash
     response = client.get(reverse("core:list-show-stash", args=[list_.id]))
-    
+
     # Check redirect
     assert response.status_code == 302
     assert response.url == reverse("core:list", args=[list_.id])
-    
+
     # Check stash fighter was created
     stash_fighter = ListFighter.objects.get(list=list_, content_fighter__is_stash=True)
     assert stash_fighter.name == "Stash"
@@ -83,9 +83,10 @@ def test_show_stash_does_not_duplicate():
     user = User.objects.create_user(username="testuser", password="testpass")
     house = ContentHouse.objects.create(name="Test House")
     list_ = List.objects.create(name="Test List", content_house=house, owner=user)
-    
+
     # Create an existing stash fighter
     from gyrinx.content.models import ContentFighter
+
     stash_content = ContentFighter.objects.create(
         house=house,
         is_stash=True,
@@ -99,17 +100,17 @@ def test_show_stash_does_not_duplicate():
         list=list_,
         owner=user,
     )
-    
+
     # Login
     client = Client()
     client.login(username="testuser", password="testpass")
-    
+
     # Call show_stash
     response = client.get(reverse("core:list-show-stash", args=[list_.id]))
-    
+
     # Check redirect
     assert response.status_code == 302
-    
+
     # Check only one stash fighter exists
     stash_count = list_.listfighter_set.filter(content_fighter__is_stash=True).count()
     assert stash_count == 1
@@ -120,18 +121,18 @@ def test_show_stash_requires_ownership():
     """Test that show_stash requires the user to own the list"""
     # Create two users
     owner = User.objects.create_user(username="owner", password="testpass")
-    other_user = User.objects.create_user(username="other", password="testpass")
-    
+    User.objects.create_user(username="other", password="testpass")
+
     # Create a list owned by the first user
     house = ContentHouse.objects.create(name="Test House")
     list_ = List.objects.create(name="Test List", content_house=house, owner=owner)
-    
+
     # Login as the other user
     client = Client()
     client.login(username="other", password="testpass")
-    
+
     # Try to call show_stash
     response = client.get(reverse("core:list-show-stash", args=[list_.id]))
-    
+
     # Should return 404
     assert response.status_code == 404
