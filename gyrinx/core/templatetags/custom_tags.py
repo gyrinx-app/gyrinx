2710124b880b16c215d051b3e5f83e3643978168
Tom Ashworth <tgvashworth@gmail.com>
2025-07-23 15:51:40 +0100

    Fix our 404 woes once and for all

--- gyrinx/core/templatetags/custom_tags.py
+++ gyrinx/core/templatetags/custom_tags.py
@@ -9,6 +9,7 @@ from django.conf import settings
 from django.core.cache import cache
 from django.template.context import RequestContext
 from django.urls import resolve
+from django.urls.exceptions import Resolver404
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
@@ -20,7 +21,13 @@ register = template.Library()
 
 def is_active(context: RequestContext, name):
     """Check if the current view is active."""
-    return name == resolve(context.request.path).view_name
+    try:
+        match = resolve(context.request.path)
+        return name == match.view_name
+    except Resolver404:
+        # We can't resolve the request path (e.g. on a 404 page!)
+        # so it can't be active.
+        return False
 
 
 @register.simple_tag(takes_context=True)
@@ -250,6 +257,41 @@ def random_404_message():
         "My cyber-mastiff ate it.",
         "Check your dome runner map is the right way up.",
         "Someone might have been at it with a data-thief.",
+        "Guess it wasn't master-crafted after all.",
+        "The engiseer said it was 'working as intended'.",
+        "Nobody knows what happened to it.",
+        "Try adjusting your augmetics.",
+        "Lost in the Ash Wastes without a rebreather.",
+        "Your data-slate was confiscated by Palanite Enforcers.",
+        "Corpse Grinders ate this page. And the server.",
+        "The Redemptionists burned it for heresy.",
+        "Sold to the Mercator Slavers for two credits.",
+        "Currently being digested by a sump kroc.",
+        "Kal Jericho shot first. The page didn't make it.",
+        "Van Saar's tech failed. Again.",
+        "I was trying out some chems and accidentally dissolved the data.",
+        "It fell through a rusted gantry.",
+        "The Genestealers doesn't want you to see this.",
+        "Scragged by a juve on their first raid.",
+        "Your access codes expired 10,000 years ago.",
+        "Currently under quarantine.",
+        "Hunted for sport.",
+        "There was an unscheduled rapid disassembly of the data shaft.",
+        "Lost in transit.",
+        "Eaten by rats. Big ones. With extra eyes.",
+        "The manufactorum quota doesn't include this page.",
+        "Chaos corruption detected. Purge initiated.",
+        "Traded for a rusty stub gun.",
+        "House Cawdor recycled it into a holy flamer.",
+        "The Guilders repo'd it for unpaid tolls.",
+        "A rogue servitor deleted the cache.",
+        "Caught in the crossfire?",
+        "Your dome runner gave you the wrong coordinates.",
+        "Our exovator is still digging for it.",
+        "Currently fermenting in a Goliath still.",
+        "The arbitrator ruled against your access request.",
+        "Lost during the Fall of Hive Secundus.",
+        "Venators claimed the bounty on this page.",
     ]
     return random.choice(messages)  # nosec
 
