708a8aefae3100fe1b4211f7db5f630bab9f9d0b
Tom Ashworth <tgvashworth@gmail.com>
2025-08-02 08:31:01 +0100

    Fix anon list routing

--- gyrinx/core/views/print_config.py
+++ gyrinx/core/views/print_config.py
@@ -36,7 +36,7 @@ class PrintConfigIndexView(generic.ListView):
         # Redirect anonymous users to the default print page
         if not request.user.is_authenticated:
             list_id = kwargs["list_id"]
-            return redirect("core:list-print", list_id=list_id)
+            return redirect("core:list-print", id=list_id)
 
         self.list = get_object_or_404(List, id=kwargs["list_id"], owner=request.user)
         return super().dispatch(request, *args, **kwargs)
